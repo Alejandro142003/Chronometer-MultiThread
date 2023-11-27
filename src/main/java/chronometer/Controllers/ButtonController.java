86ac13e14d4164fa19878394f6c98bc900b26ddb
Alejandro <gutierrezcrucesalejandro@gmail.com>
2023-11-27 21:49:42 +0100

    Arreglos finales

---
+++ src/main/java/chronometer/Controllers/ButtonController.java
@@ -0,0 +1,5 @@
+package chronometer.Controllers;
+
+//No se que hace esta clase aquí pero si lo borro no funciona nada
+public class ButtonController {
+}
--- src/main/java/chronometer/Controllers/ButtonsController.java
+++
@@ -1,5 +0,0 @@
-package chronometer.Controllers;
-
-public class ButtonsController {
-    
-}
